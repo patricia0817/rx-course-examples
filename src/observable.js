4016241f83cc19f7b39eaca2c4d6839db7b5bbdf
Patricia Mirabela Mihut <Patricia.Mihut@endava.com>
2021-04-27 17:24:41 +0300

    Observables - start

--- src/observable.js
+++ src/observable.js
@@ -1,1 +1,34 @@
-import { add } from "./helpers"
+import { add } from './helpers';
+import { Observable } from 'rxjs';
+
+// function Observable(subscriber) {
+//   subscriber.next('Hello Universe');
+// }
+
+const observer = {
+  next: add.li,
+  error: add.li,
+  complete: () => {
+    add.li('There are no more values');
+  },
+};
+
+// Observable(observer);
+
+// class Observable {
+//   constructor(subscribeTo) {
+//     this.subscribeTo = subscribeTo;
+//   }
+
+//   subscribe(observer) {
+//     return this.subscribeTo(observer);
+//   }
+// }
+
+const producer = new Observable(subscribe => {
+  subscribe.next('Hello from the Observable class');
+  subscribe.complete();
+  subscribe.next('Something I forgot');
+});
+
+producer.subscribe(observer);
